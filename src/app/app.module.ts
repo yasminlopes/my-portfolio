89b5029b6b4ffded90e21711107d2a1b19148cd6
Yasmin Lopes <yclopes27@gmail.com>
2023-10-04 14:24:01 -0300

    feat: setup translate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NavbarComponent,
     BrowserAnimationsModule,
+    HttpClientModule,
+    FeaturesModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json'),
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
