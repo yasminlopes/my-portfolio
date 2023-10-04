d0889e91049ca5eff97f07c20036996040603a87
Yasmin Lopes <yclopes27@gmail.com>
2023-10-04 11:41:12 -0300

    feat: set title and meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'yasmin-teste';
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Yasmin Lopes | Desenvolvedora Front-end');
+
+    this.metaService.addTags([
+      { name: 'keywords', content: 'Frontend, software, developer' },
+      {
+        name: 'description',
+        content:
+          'Amo a tecnologia, e o Design. Unindo os dois, crio ideias e desenvolvo soluções usando minha criatividade e meu conhecimento.',
+      },
+    ]);
+  }
 }
