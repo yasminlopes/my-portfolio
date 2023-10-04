fe96f55d7273a61b9ec18cc64018135bec51bc25
Yasmin Lopes <yclopes27@gmail.com>
2023-10-04 14:28:05 -0300

    feat: implement translations methods

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { NAVBAR_ITEMS, Navbar } from '../../models/navbar.model';
+import {
+  LANGUAGE_DROPDOWN_ITEMS,
+} from '../../models/navbar.model';
+import {
+  ButtonDropdownComponent,
+  ItemDropdown,
+} from 'src/app/shared/components/button-dropdown/button-dropdown.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { lastValueFrom } from 'rxjs';
+import { IpApiService } from 'src/app/shared/services/ip-api/ip-api.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink, NgOptimizedImage],
+  imports: [
+    CommonModule,
+    RouterLink,
+    NgOptimizedImage,
+    ButtonDropdownComponent,
+    TranslateModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
@@ -14,11 +29,44 @@ export class NavbarComponent {
   public languageDropdown = LANGUAGE_DROPDOWN_ITEMS;
   public isMenuOpen: boolean = false;
 
+  private _currentLanguage: string = 'br';
+
+  constructor(
+    private _translate: TranslateService,
+    private _apiService: IpApiService
+  ) {}
+
   public toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  async ngOnInit() {
+    await this.setLanguage();
+  }
+
+  private async setLanguage() {
+    const ipInfo$ = this._apiService.getIpInfo();
+    const ipInfo = await lastValueFrom(ipInfo$);
+
+    this._translate.setDefaultLang('en');
+    
+    if (ipInfo?.country_code?.toUpperCase() == 'BR') {
+      this._translate.setDefaultLang('pt');
+      this._currentLanguage = 'pt';
+    }
+  }
+
   public executeEventDropdown(item: ItemDropdown) {
     this[item.onClickFunction](item, item.value);
+  }
+
+  private changeToEnglish(): void {
+    this._translate.use('en');
+    this._currentLanguage = 'en';
+  }
+
+  private changeToPortuguese(): void {
+    this._translate.use('pt');
+    this._currentLanguage = 'pt';
   }
 }
